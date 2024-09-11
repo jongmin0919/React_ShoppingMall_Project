cd2a50738ea0e62a15ec936910c8d4765ee4a312
jongmin0919 <jongmin0919@naver.com>
2024-09-11 20:09:11 +0900

    MUI converting2

--- version4_material/client/src/pages/ProductPage.tsx
+++ version4_material/client/src/pages/ProductPage.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { API_SERVER_DOMAIN } from "../constants";
-import { Box } from "@mui/material";  // Material-UI 컴포넌트 추가
+import { ProductType } from "../types";
+import { Box, Button, ButtonGroup, Typography } from "@mui/material";  // Material-UI 컴포넌트 추가
+import { Delete, Edit } from "@mui/icons-material";
 
 function ProductPage() {
-  type ProductType = {
-    id: string;
-    name: string;
-    explanation: string;
-    price: number;
-    thumbnail?: string; // Optional thumbnail field 추가
-  };
-
+  
+  const navigate = useNavigate();
   const { productId } = useParams<{ productId: string }>();
   const [product, setProduct] = useState<ProductType | null>(null);
+
+  const handlePushPurchasePage = () => {
+    if(productId){
+      navigate(`/purchase/${productId}`)
+    }
+  }
 
   useEffect(() => {
     fetch(`/product/${productId}`)
@@ -35,20 +37,45 @@ function ProductPage() {
   }
 
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", mb: 4 }}>
-      {product?.thumbnail && (
-        <img
-          src={`${API_SERVER_DOMAIN}/${product.thumbnail}`}
-          alt={product?.name}
-          style={{ width: "100%", maxWidth: 400 }}
-        />
-      )}
-      <div>
-        <h1>{product?.name}</h1>
-        <p>{product?.explanation}</p>
-        <span>{product?.price}</span>
-      </div>
-    </Box>
+    <>
+      <Box sx={{ display: "flex", justifyContent: "center", mb: 4 }}>
+        {product?.thumbnail && (
+          <img
+            src={`${API_SERVER_DOMAIN}/${product.thumbnail}`}
+            alt={product?.name}
+            style={{ width: "100%", maxWidth: 400 }}
+          />
+        )}
+      </Box>
+      <Box sx = {{display: "flex", alignItems : "center", justifyContent : "space-between", marginBottom : 2}}>
+        <Typography variant = "h4" sx = {{fontWeight : 'bold'}}>
+          {product?.name}
+        </Typography>
+        <ButtonGroup orientation = "horizontal">
+          <Button variant = "text" onClick = {() => null} color = "error">
+            <Delete/>
+          </Button>
+          <Button variant = "text" onClick = {() => null} color = "info">
+            <Edit/>
+          </Button>
+        </ButtonGroup>
+      </Box>
+      <Typography variant = "h6" sx = {{marginBottom : 4}}>
+        {product?.price.toLocaleString('KO-kr')} 원
+      </Typography>
+      <Typography variant = "body1" sx = {{marginBottom : 4}}>
+        {product?.explanation}
+      </Typography>
+
+      <ButtonGroup orientation = "vertical" fullWidth>
+          <Button variant = "outlined">
+            장바구니 담기
+          </Button>
+          <Button variant = "contained" onClick={handlePushPurchasePage}>
+            구매하기
+          </Button>
+        </ButtonGroup>
+    </>
   );
 }
 
